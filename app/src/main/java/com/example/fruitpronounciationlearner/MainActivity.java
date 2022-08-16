05c37a68224dfc8ba162e55ab4a87384f96fd6fb
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2022-08-17 00:07:13 +0500

    Added custom adapter

--- app/src/main/java/com/example/fruitpronounciationlearner/MainActivity.java
+++ app/src/main/java/com/example/fruitpronounciationlearner/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String fruitName [] = {"Apple", "Apricot", "Banana", "Guava", "Mango", "Orange", "Pineapple", "Pomegranate"};
+    int fruitImage [] = {R.drawable.apple,R.drawable.apricot, R.drawable.banana, R.drawable.guava, R.drawable.mango, R.drawable.orange, R.drawable.pineapple, R.drawable.pomegranate};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
