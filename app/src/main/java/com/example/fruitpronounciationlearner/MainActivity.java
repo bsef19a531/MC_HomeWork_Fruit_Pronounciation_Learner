b8581ce379ce097cafc05378bc7c99cbc6231bae
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2022-08-17 00:53:26 +0500

    Completed Custom ListView Logic

--- app/src/main/java/com/example/fruitpronounciationlearner/MainActivity.java
+++ app/src/main/java/com/example/fruitpronounciationlearner/MainActivity.java
@@ -3,21 +3,23 @@ package com.example.fruitpronounciationlearner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     String fruitName [] = {"Apple", "Apricot", "Banana", "Guava", "Mango", "Orange", "Pineapple", "Pomegranate"};
     int fruitImage [] = {R.drawable.apple,R.drawable.apricot, R.drawable.banana, R.drawable.guava, R.drawable.mango, R.drawable.orange, R.drawable.pineapple, R.drawable.pomegranate};
+    ListView fruitList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView fruitList;
-        fruitList = (ListView) findViewById(R.layout.activity_custom_list_view);
+
         CustomListAdapter listAdapter = new CustomListAdapter(this, fruitName, fruitImage);
+        fruitList = (ListView) findViewById(R.id.customlistview);
         fruitList.setAdapter(listAdapter);
     }
 }
