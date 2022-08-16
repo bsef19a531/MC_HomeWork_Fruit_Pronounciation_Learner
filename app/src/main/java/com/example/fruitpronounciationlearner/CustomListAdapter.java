b568aa9aa854dde06402c2dfd0e4a47c0745f6e9
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2022-08-17 00:25:32 +0500

    Finalized CustomAdapter

--- app/src/main/java/com/example/fruitpronounciationlearner/CustomListAdapter.java
+++ app/src/main/java/com/example/fruitpronounciationlearner/CustomListAdapter.java
@@ -1,10 +1,26 @@
 package com.example.fruitpronounciationlearner;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CustomListAdapter extends BaseAdapter {
+    Context ctx;
+    String [] Names;
+    int [] Images;
+    LayoutInflater inflater;
+
+    public CustomListAdapter(Context ctx, String [] Names, int [] Images)
+    {
+        this.ctx= ctx;
+        this.Names = Names;
+        this.Images = Images;
+        this.inflater = LayoutInflater.from(ctx);
+    }
 
     @Override
     public int getCount() {
@@ -23,7 +39,12 @@ public class CustomListAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
-        
+        view = inflater.inflate(R.layout.activity_custom_list_view, null);
+        TextView txtView = (TextView) view.findViewById(R.id.textView);
+        ImageView imgView = (ImageView) view.findViewById(R.id.imageIcon);
+        txtView.setText(Names[i]);
+        imgView.setImageResource(Images[i]);
+        return view;
+
     }
 }
